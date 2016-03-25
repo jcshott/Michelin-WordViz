5b6ce3a5680716beebce613f0ecbc1cb49c03d80
Corey Shott <jcshott@gmail.com>
2016-03-25 08:49:19 -0700

    starting to change from dumping review info into json to dumping to db

--- bs_scrape.py
+++ bs_scrape.py
@@ -1,4 +1,5 @@
-import json
+from models import connect_to_db, db, Reviews, Restaurants
+from runserver import app
 import os
 import random
 import re
@@ -7,8 +8,15 @@ import urllib2
 
 from bs4 import BeautifulSoup
 
+## TODO: Add restaurant to db in the grab-review-obj fn. add reviews to db in dump reviews fn
 
 def grab_review_objects(search_url, restaurant_name):
+    """
+        Grabs all reviews from given restaurant's yelp page.
+
+    """
+    restaurant_info = {}
+
     rest_time = random.randint(5000, 60000)/1000.0 # before scraping the page, script will rest for between 5 to 60 secs
     print "rest time is", rest_time # print statements to see scraping progress
     time.sleep(rest_time)
@@ -16,14 +24,15 @@ def grab_review_objects(search_url, restaurant_name):
 
     contents = urllib2.urlopen(search_url).read()
     soup = BeautifulSoup(contents, "html.parser")
-    biz_id = soup.find('a', attrs={'class':'edit-category', 'href':True}).attrs['href'] # grab the yelp biz id
-    biz_id = biz_id.replace("/biz_attribute?biz_id=", "") # lop off the prefix
-    name = (soup.find('h1', attrs={'itemprop':'name'}).get_text()).strip()
-    print name
-    yelp_name = restaurant_name
-    avg_rating = float(soup.find('meta', {'itemprop':'ratingValue', 'content':True}).attrs['content'])
-    location = soup.find('span', attrs={'itemprop':'addressLocality'}).get_text() + ", " + soup.find('span', attrs={'itemprop':'addressRegion'}).get_text()
-    stars = 3
+
+    if not restaurant_info: # only need to grab this stuff the first time through review scraping
+        biz_id = soup.find('a', attrs={'class':'edit-category', 'href':True}).attrs['href'] # grab the yelp biz id
+        restaurant_info['biz_id'] = biz_id.replace("/biz_attribute?biz_id=", "") # lop off the prefix
+        restaurant_info['name'] = (soup.find('h1', attrs={'itemprop':'name'}).get_text()).strip()
+        restaurant_info['yelp_name'] = restaurant_name
+        restaurant_info["avg_rating"] = float(soup.find('meta', {'itemprop':'ratingValue', 'content':True}).attrs['content'])
+        restaurant_info["location"] = soup.find('span', attrs={'itemprop':'addressLocality'}).get_text() + ", " + soup.find('span', attrs={'itemprop':'addressRegion'}).get_text()
+        restaurant_info["stars"] = 3
 
     next = soup.find('a', attrs={'class':'next'}) # look for the "next" page link
     
@@ -37,15 +46,34 @@ def grab_review_objects(search_url, restaurant_name):
     if next:
         next_page_reviews, _ = grab_review_objects(next.attrs['href'], restaurant_name)
 
+    
+    load_restaurant_info_to_db(restaurant_info)
+    
     return review_objs_list + next_page_reviews, biz_id
 
+def load_restaurant_info_to_db(restaurant_info):
 
-def create_review_dicts(restaurant, existing_dict):
+    biz_id = restaurant_info["biz_id"]
+    name = restaurant_info["name"]
+    yelp_name = restaurant_info["yelp_name"]
+    avg_rating = float(restaurant_info["avg_rating"])
+    location = restaurant_info["location"]
+    stars = int(restaurant_info["stars"])
+
+    temp_rest_obj = Restaurants(biz_id=biz_id, common_name=name, yelp_name=yelp_name, location=location, avg_yelp_rating=avg_rating, michelin_stars=stars)
+
+    db.session.add(temp_rest_obj)
+    db.session.commit()
+    print "restaurant info added!"
+
+
+def load_reviews_to_db(restaurant):
+
     search_url = search_base_url + restaurant + search_suffix
     review_objs_list, biz_id = grab_review_objects(search_url, restaurant)
     review_dicts_20 = []
     for review in review_objs_list:
-        review_dict = {}
+        
         review_dict['review_id'] = review.attrs['data-review-id']
         review_dict['date'] = review.find('meta', attrs={'itemprop':'datePublished', 'content':True}).attrs['content']
         review_dict['rating'] = float(review.find('meta', {'itemprop':'ratingValue', 'content':True}).attrs['content'])
@@ -55,49 +83,22 @@ def create_review_dicts(restaurant, existing_dict):
         review_dict['biz_id'] = biz_id # primary key in restaurants table, foreign key in reviews table
         review_dicts_20.append(review_dict)
 
-    return existing_dict + review_dicts_20
 
+def main(restaurant):
+    load_reviews_to_db(restaurant)
 
-def load_file(restaurant_name):
-    exiting_file = os.path.exists("json/%s.json" % restaurant_name) # make sure you have a json/ folder on the same level as this file
-    if exiting_file:
-        f = open("json/%s.json" % restaurant_name, "r")
-        review_dicts = json.load(f)
-        f.close()
-    else:
-        review_dicts = []
-    return review_dicts
-
-
-def dump_to_file(restaurant_name, review_dicts):
-    """ takes 2 args: 
-        -name of restaurant - compute name of file
-        -list of reviews - all + 20 new ones
-        then figures name out name of file, opens it
-        and dumps review_dicts into it """
-
-    f = open("json/%s.json" % restaurant_name, "w+")
-    json.dump(review_dicts, f, indent=4, separators=(", ", ": "))
-    f.close()
-
-
-def main(restaurant_name, restaurant):
-    review_dicts = load_file(restaurant_name)
-    review_dicts = create_review_dicts(restaurant, review_dicts)
-    dump_to_file(restaurant_name, review_dicts)
 
 if __name__ == "__main__":
     
-    search_base_url = "https://www.yelp.com/biz/"
-    
-    # list of tuples (yelp name, name for file) to scrape multiple restaurants in one run
-    # where tuple[0] = restaurant (as listed in yelp url (aka the yelp id)); tuple[1] = short name for output file
-    restaurant_list = [("jean-georges-new-york", "jeanGeorges"), ("chefs-table-at-brooklyn-fare-brooklyn-3", "brooklynFare")]
+    connect_to_db(app)
 
-    # restaurant = "benu-san-francisco-4" # change this to the correct retaurant
-    # restaurant_name = "benu" # useful for creating/naming the json file or sql table
+    # needed to construct URLs for scraping
+    search_base_url = "https://www.yelp.com/biz/"
     search_suffix = "?sort_by=date_desc"
     starting_url = "https://www.yelp.com"
+    
+    # the 3 star, US Michelin restaurants to gather data from yelp & dump to db
+    restaurant_list = ["jean-georges-new-york", "chefs-table-at-brooklyn-fare-brooklyn-3", "per-se-new-york", "masa-new-york"]
 
     for item in restaurant_list:
-        main(item[1], item[0])
+        main(item)
