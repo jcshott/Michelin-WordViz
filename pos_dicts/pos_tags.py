6349dbf32921aa62c0a1d30731309cd361fb766c
Antonia Wang <antonia.t.wang@gmail.com>
2016-04-15 10:36:22 -0700

    added list of POS to keep, remove and maybes

---
+++ pos_dicts/pos_tags.py
@@ -0,0 +1,5 @@
+pos_tags = {
+	'keep': ['VBG', 'VDB', 'VBN', 'VBP', 'JJ', 'VBZ' 'NN', 'FW', 'RB', 'NNS', 'VB', 'RBR', 'JJS', 'JJR'],
+	'maybe': ['SYM', 'UH']
+	'remove': ['PRP$', 'WDT', 'WP', 'DT', 'RP', 'POS', 'PRP', 'WRB', 'CC', 'PDT', 'CD', 'EX', 'IN', 'WP$', 'MD']
+}
