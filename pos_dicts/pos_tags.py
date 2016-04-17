ce1208f65f47e5c8e30ba6c8f6318ce974ff3115
Corey Shott <jcshott@gmail.com>
2016-04-17 16:16:43 -0700

    added a few tags to remove based on looking at a few more restaurant outputs

--- pos_dicts/pos_tags.py
+++ pos_dicts/pos_tags.py
@@ -1,5 +1,8 @@
 pos_tags = {
-	'keep': ['VBG', 'VDB', 'VBN', 'VBP', 'JJ', 'VBZ' 'NN', 'FW', 'RB', 'NNS', 'VB', 'RBR', 'JJS', 'JJR'],
+	'keep': ['VBG', 'VBD', 'VBN', 'VBP', 'JJ', 'VBZ' 'NN', 'FW', 'RB', 'NNS', 'VB', 'RBR', 'JJS', 'JJR'],
 	'maybe': ['SYM', 'UH']
-	'remove': ['PRP$', 'WDT', 'WP', 'DT', 'RP', 'POS', 'PRP', 'WRB', 'CC', 'PDT', 'CD', 'EX', 'IN', 'WP$', 'MD']
+	'remove': ['PRP$', 'WDT', 'WP', 'DT', 'RP', 'POS', 'PRP', 'WRB', 'CC', 'PDT', 'CD', 'EX', 'IN', 'WP$', 'MD',' VBN', 'RP', 'TO', 'NNP', 'NNPS', 'RBS', 'LS']
 }
+
+
+# reference: https://sites.google.com/site/partofspeechhelp/home
